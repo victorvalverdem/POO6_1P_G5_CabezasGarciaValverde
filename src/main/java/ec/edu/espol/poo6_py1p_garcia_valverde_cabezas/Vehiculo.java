6b9da1b1cfa46ec8cf80ffaeb020d41c528d3b8b
stephcarol <scabezas@espol.edu.ec>
2021-11-19 23:14:59 -0500

    actualizacion de la clase Vehiculo

--- src/main/java/ec/edu/espol/poo6_py1p_garcia_valverde_cabezas/Vehiculo.java
+++ src/main/java/ec/edu/espol/poo6_py1p_garcia_valverde_cabezas/Vehiculo.java
@@ -14,6 +14,50 @@ public class Vehiculo {
     private String modelo;
     private String marca;
     private TipoVehiculo tipoVeh;
+
+    public Vehiculo(String placa, String modelo, String marca, TipoVehiculo tipoVeh) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.tipoVeh = tipoVeh;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public TipoVehiculo getTipoVeh() {
+        return tipoVeh;
+    }
+
+    public void setTipoVeh(TipoVehiculo tipoVeh) {
+        this.tipoVeh = tipoVeh;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "placa=" + placa + ", modelo=" + modelo + ", marca=" + marca + ", tipoVeh=" + tipoVeh + '}';
+    } 
     
     
     
