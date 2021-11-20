e0e37d7b6b02a8821e9af112a8a88075895bd97a
stephcarol <85514342+stephcarol@users.noreply.github.com>
2021-11-19 22:41:52 -0500

    Add files via upload

---
+++ POO6_PY1P_Garcia_Valverde_Cabezas/src/main/java/ec/edu/espol/poo6_py1p_garcia_valverde_cabezas/Vehiculo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.poo6_py1p_garcia_valverde_cabezas;
+
+/**
+ *
+ * @author USER
+ */
+public class Vehiculo {
+    private String placa;
+    private String modelo;
+    private String marca;
+    private TipoVehiculo tipoVeh;
+    
+    
+    
+}
